b98e2499d1e4aef0845e99e7bf4c5154a525dec6
Danil <kokudak292@gmail.com>
2025-04-02 17:46:55 +0200

    added another repository with images

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from kivy.app import App
-
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -8,12 +7,12 @@ from kivy.uix.image import Image
 class LikeApp(App):
     def build(self):
         self.mainBox = BoxLayout(orientation="vertical")
-        self.lbl_title  = Label(text='rate this picture', font_size= 30)
+        self.lbl_title  = Label(text='Rate this picture', font_size=30)
         self.image = Image(source="f11cfe34ed1a91e120326b2ea06296b2.jpg")
-        btn_layout = BoxLayout(size_hint =[1,0.2])
+        btn_layout = BoxLayout(size_hint=[1, 0.2])
 
-        btn_like = Button(text="Like",font_size=24,size_hint=[0.45,0.7],)
-        btn_dislike = Button(text="Like",font_size=24,size_hint=[0.45,0.7],)
+        btn_like = Button(text="Like", font_size=24, size_hint=[0.45, 0.7])
+        btn_dislike = Button(text="Dislike", font_size=24, size_hint=[0.45, 0.7])
 
         self.mainBox.add_widget(self.lbl_title)
         self.mainBox.add_widget(self.image)
@@ -23,5 +22,15 @@ class LikeApp(App):
 
         self.mainBox.add_widget(btn_layout)
 
+        btn_like.bind(on_press=self.like)
+        btn_dislike.bind(on_press=self.dislike)
+
         return self.mainBox
+
+    def like(self, btn):
+        print('Liked')
+
+    def dislike(self, btn):
+        print('Disliked')
+
 LikeApp().run()
